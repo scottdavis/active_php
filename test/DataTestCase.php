44173c7bafbffa458333a2058d367a63fe09cde0
Scott Davis <sdavis@thorn.stsci.edu>
2009-05-29 17:07:02 -0400

    worked on more teting

--- test/DataTestCase.php
+++ test/DataTestCase.php
@@ -1,20 +1,18 @@
 <?php
 
-abstract class DataTestCase extends PHPUnit_Framework_TestCase {
+class DataTestCase extends PHPUnit_Framework_TestCase {
 	
 	
 	public function setUp() {
 		ActivePhp\Base::$test_mode = true;
 		User::start_transaction();
-		if(method_exists($this, 'setup')) {
-			call_user_func(array($this, 'setup'));
-		}
+	}
+	
+	public function testFoo() {
+		$this->assertTrue(true);
 	}
 	
 	public function tearDown() {
-		if(method_exists($this, 'teardown')) {
-			call_user_func(array($this, 'teardown'));
-		}
 		User::end_transaction();
 		ActivePhp\Base::$test_mode = false;
 	}

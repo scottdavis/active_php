32fe3e8975d9eeb5c69fce2865597dec281dd939
Scott Davis <sdavis@thorn.stsci.edu>
2009-06-01 15:31:02 -0400

    ok returning null when a record is null now works!

--- test/UpdateTest.php
+++ test/UpdateTest.php
@@ -50,7 +50,7 @@ require_once('TestSetup.php');
 		//reload joe
 		$joe2 = User::_find($joe->id);
 		//assert that nothing changed
-		$this->assertTrue(empty($joe2->my_int));
+		$this->assertTrue(is_null($joe2->my_int));
 	}
 	
 	

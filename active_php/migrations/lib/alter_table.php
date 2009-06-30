b20eea297a1a612ea9d407e0113c586122d44592
Scott Davis <srdavis@scott-daviss-macbook-pro.local>
2009-06-29 23:13:36 -0400

    redefined a function twice ... that was bad

--- active_php/migrations/lib/alter_table.php
+++ active_php/migrations/lib/alter_table.php
@@ -2,21 +2,19 @@
 	class AlterTable {
 		
 		public function __construct($table_name, $options, $migration) {
-				public function __construct($table_name, $options, $migration) {
-					$this->passed_options = $options;
-					$this->migration = $migration;
-					$this->table_name = $table_name;
-					$this->quoted_table_name = Migration::quote_table_name($table_name);
-					$this->sql = 'ALTER TABLE ' . $this->quoted_table_name;
-					$this->columns = array();
-					$this->options = "ENGINE=InnoDB DEFAULT CHARSET=utf8";
-					$this->other = array();
+				$this->passed_options = $options;
+				$this->migration = $migration;
+				$this->table_name = $table_name;
+				$this->quoted_table_name = Migration::quote_table_name($table_name);
+				$this->sql = 'ALTER TABLE ' . $this->quoted_table_name;
+				$this->columns = array();
+				$this->options = "ENGINE=InnoDB DEFAULT CHARSET=utf8";
+				$this->other = array();
 
-					if(isset($this->passed_options['id']) && !$this->passed_options['id']) {
-						//do nothing
-					}else{
-						array_push($this->columns, "id int(11) DEFAULT NULL auto_increment PRIMARY KEY");
-					}
+				if(isset($this->passed_options['id']) && !$this->passed_options['id']) {
+					//do nothing
+				}else{
+					array_push($this->columns, "id int(11) DEFAULT NULL auto_increment PRIMARY KEY");
 				}
 		}
 		
